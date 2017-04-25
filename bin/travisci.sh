ebdccde9040d6c89d02b50cb582b3f293179bbd5
David Williams-Young <dbwy@uw.edu>
2017-04-25 08:31:45 -0700

    Travis build defaults to -O3 to avoid timout

--- bin/travisci.sh
+++ bin/travisci.sh
@@ -5,14 +5,12 @@ set -e
 if [ "$CXX" = "g++" ]; then
     export CC=/usr/bin/gcc-$GCC_VERSION
     export CXX=/usr/bin/g++-$GCC_VERSION
-    export OPENMPFLAGS=-fopenmp
 else
     # no OpenMP support in clang, will use C++11 threads
-    export OPENMPFLAGS=
     export CC=/usr/bin/clang-3.8
     export CXX=/usr/bin/clang++-3.8
 fi
-export CXXFLAGS="-std=c++11"
+export CXXFLAGS="-std=c++11 -O3"
 
 mkdir build && cd build
 cmake -DBOOST_LIBRARYDIR='/usr/lib' ..
