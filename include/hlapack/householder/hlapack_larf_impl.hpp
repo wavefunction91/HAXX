ce78544dae8ea36e8d4e42962081aace4e4657be
David Williams-Young <dbwy@uw.edu>
2017-05-04 08:33:16 -0700

    Added HLAPACK function to apply a quaternion Householder reflector, currently only testing left application

---
+++ include/hlapack/householder/hlapack_larf_impl.hpp
@@ -0,0 +1,48 @@
+/*
+ *  This file is a part of HAXX
+ *  
+ *  Copyright (c) 2017 David Williams-Young
+ *  All rights reserved.
+ *  
+ *  See LICENSE.txt 
+ */
+
+#ifndef __INCLUDED_HLAPACK_LARF_IMPL_HPP
+#define __INCLUDED_HLAPACK_LARF_IMPL_HPP
+
+#include "hlapack/householder_def.hpp"
+
+namespace HAXX {
+
+/**
+ *  Written by DBWY (5/2017)
+ *
+ *  Naming convention and interface based on LAPACK implementation
+ *  of ZLARF. Implementation based in part on Algorithm A2 of
+ *
+ *    Bunse-Gerstner, A.; Byers, R.; Mehrmann, V.; 
+ *      ``A Quaternion QR Algorithm"; Numer. Math. 55, 83-95 (1989)
+ */
+template < typename _F >
+void HLAPACK_LARF(char SIDE, HAXX_INT M, HAXX_INT N, quaternion<_F> *V,
+  HAXX_INT INCV, quaternion<_F> TAU, quaternion<_F> *C, HAXX_INT LDC,
+  quaternion<_F> *WORK) {
+
+  if( SIDE == 'L' ) {
+    // FIXME: This is stupid, should generalize GEMV to multiply the
+    // vector from the left and to allow for conjugation of X. 
+    //   This implementaion assumes that INCV == 1
+    assert( INCV == 1 );
+
+    HBLAS_GEMM('C','N',1,N,M,_F(1.),V,M,C,LDC,_F(0.),WORK,1);
+    HBLAS_GERU(M,N,-TAU,V,INCV,WORK,1,C,LDC);
+  } else {
+
+    HBLAS_GEMV('N',M,N,_F(1.),C,LDC,V,INCV,_F(0.),WORK,1);
+    HBLAS_GERC(M,N,-TAU,WORK,1,V,INCV,C,LDC);
+
+  }
+}
+
+}; // namespace HAXX
+#endif
--- src/unit_test/haxx_hlapack_householder.cxx
+++ src/unit_test/haxx_hlapack_householder.cxx
@@ -3,9 +3,13 @@
 
 #include "haxx_ut.hpp"
 #include "hblas/hblas1_impl.hpp"
+#include "hblas/hblas2_impl.hpp"
+#include "hblas/hblas3_impl.hpp"
 #include "hlapack/householder_impl.hpp"
 
 
+//#undef HBLAS1_VECLEN
+//#define HBLAS1_VECLEN 10
 BOOST_AUTO_TEST_CASE(hlapack_larfg)
 {
   std::vector<HAXX::quaternion<double>> 
@@ -15,30 +19,20 @@ BOOST_AUTO_TEST_CASE(hlapack_larfg)
   for(auto &x : X) 
     x = HAXX::quaternion<double>(dis(gen),dis(gen),dis(gen),dis(gen));
 
-/*
-  for(auto &x : X) 
-    std::cout << x << std::endl;
-  std::cout << std::endl; 
-*/
 
-  XC = X;
+  std::copy(X.begin(),X.end(),XC.begin());
 
   double BETA = HLAPACK_LARFG(HBLAS1_VECLEN,&X[0],1);
   
   auto Inner = HBLAS_DOTC(HBLAS1_VECLEN,&X[0],1,&XC[0],1);
-  HBLAS_SCAL('R',HBLAS1_VECLEN,Inner,&X[0],1);
-  HBLAS_AXPY('L',HBLAS1_VECLEN,-BETA,&X[0],1,&XC[0],1);
-
-/*
-  for(auto &x : XC) 
-    std::cout << x << std::endl;
-  std::cout << std::endl; 
-*/
-
   double x2nrm = HBLAS_NRM2(HBLAS1_VECLEN,&XC[0],1);
   auto   Uy    = XC[0] / HAXX::norm(XC[0]);
 
-  BOOST_CHECK(CMP_Q(x2nrm*Uy,XC[0]));
+  HBLAS_SCAL('R',HBLAS1_VECLEN,Inner,&X[0],1);
+  HBLAS_AXPY('L',HBLAS1_VECLEN,-BETA,&X[0],1,&XC[0],1);
+
+
+  BOOST_CHECK(CMP_Q(-x2nrm*Uy,XC[0]));
   for(auto j = 1; j < HBLAS1_VECLEN; ++j){
     BOOST_CHECK_SMALL(XC[j].real()  ,COMPARE_TOL);
     BOOST_CHECK_SMALL(XC[j].imag_i(),COMPARE_TOL);
@@ -46,4 +40,32 @@ BOOST_AUTO_TEST_CASE(hlapack_larfg)
     BOOST_CHECK_SMALL(XC[j].imag_k(),COMPARE_TOL);
   }
 
+}
+
+BOOST_AUTO_TEST_CASE(hlapack_larf)
+{
+  std::vector<HAXX::quaternion<double>> 
+    X(HBLAS1_VECLEN), XC(HBLAS1_VECLEN), SCR(HBLAS1_VECLEN);
+
+  
+  for(auto &x : X) 
+    x = HAXX::quaternion<double>(dis(gen),dis(gen),dis(gen),dis(gen));
+
+  std::copy(X.begin(),X.end(),XC.begin());
+
+  HAXX::quaternion<double> BETA = HLAPACK_LARFG(HBLAS1_VECLEN,&X[0],1);
+  auto Inner = HBLAS_DOTC(HBLAS1_VECLEN,&X[0],1,&XC[0],1);
+  double x2nrm = HBLAS_NRM2(HBLAS1_VECLEN,&XC[0],1);
+  auto   Uy    = XC[0] / HAXX::norm(XC[0]);
+
+  HLAPACK_LARF('L',HBLAS1_VECLEN,1,&X[0],1,BETA,&XC[0],HBLAS1_VECLEN,
+    &SCR[0]);
+
+  BOOST_CHECK(CMP_Q(-x2nrm*Uy,XC[0]));
+  for(auto j = 1; j < HBLAS1_VECLEN; ++j){
+    BOOST_CHECK_SMALL(XC[j].real()  ,COMPARE_TOL);
+    BOOST_CHECK_SMALL(XC[j].imag_i(),COMPARE_TOL);
+    BOOST_CHECK_SMALL(XC[j].imag_j(),COMPARE_TOL);
+    BOOST_CHECK_SMALL(XC[j].imag_k(),COMPARE_TOL);
+  }
 }
